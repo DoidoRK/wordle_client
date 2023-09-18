8ba1548806b92e8da085e7b283e53a51f9fcd64d
João Carlos <doidobr46@gmail.com>
2023-09-18 16:55:04 -0300

    Square print function implemented.

--- main.cc
+++ main.cc
@@ -4,33 +4,47 @@
 
 using namespace std;
 
-void printRow() {
-    // Define the size of each square
+void printSquare(int startX,int startY, char character, int color) {
     int squareSize = 5;
-
-    // Loop to print five squares side by side
-    for (int i = 0; i < 5; ++i) {
-        setfontcolor(RED);
-        // Loop to print each row of the square
-        for (int row = 0; row < squareSize; ++row) {
-            // Position the cursor to the appropriate row and column
-            gotoxy(i * (squareSize + 1) + 1, row + 1);
-            
-            // Print the first and last rows with '*' characters
-            if (row == 0 || row == squareSize - 1) {
-                for (int col = 0; col < squareSize; ++col) {
-                    cout << "*";
-                }
-            } else {
-                // Print the middle rows with '*' at the edges and spaces inside
-                cout << "*";
-                for (int col = 1; col < squareSize - 1; ++col) {
-                    cout << " ";
-                }
+    setfontcolor(color);
+    // Loop to print each row of the square
+    for (int row = startY; row < startY+squareSize; ++row) {
+        // Position the cursor to the appropriate row and column
+        gotoxy(startX * (squareSize + 1) + 1, row + 1);
+        
+        // Print the first and last rows with '*' characters
+        if (row == startY || row == startY+(squareSize - 1)) {
+            for (int col = 0; col < squareSize; ++col) {
                 cout << "*";
             }
+        } else {
+            // Print the middle rows with '*' at the edges and spaces inside
+            cout << "*";
+            for (int col = startX+1; col < startX+(squareSize - 1); ++col) {
+                if(row == startY+(squareSize/2) && col == startX+(squareSize/2)){
+                    cout << character;
+                } else {
+                    cout << " ";
+                }
+            }
+            cout << "*";
         }
     }
+}
+
+void printRow() {
+    // printSquare(0,0,'A');
+    // printSquare(1,0,'A');
+    // printSquare(2,0,'A');
+    // printSquare(3,0,'A');
+    // printSquare(4,0,'A');
+
+    printSquare(0,0,'A',RED);
+    printSquare(0,5,'A',GREEN);
+    printSquare(2,10,'A',YELLOW);
+    printSquare(3,15,'A',WHITE);
+    printSquare(4,20,'A',RED);
+    
 }
 
 int main() {
