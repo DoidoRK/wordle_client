5713e08164fc39bdc6c718d4246737bc281b7765
João Carlos <doidobr46@gmail.com>
2023-09-18 17:18:29 -0300

    Word Exhibition menu almost done

--- main.cc
+++ main.cc
@@ -3,17 +3,17 @@
 #include <unistd.h> 
 
 using namespace std;
+int squareSize = 5;
 
-void printSquare(int startX,int startY, char character, int color) {
-    int squareSize = 5;
+void printSquare(int startX, int startY, char character, int color) {
     setfontcolor(color);
     // Loop to print each row of the square
-    for (int row = startY; row < startY+squareSize; ++row) {
+    for (int row = startY*(squareSize+1); row < startY*(squareSize+1)+squareSize; ++row) {
         // Position the cursor to the appropriate row and column
-        gotoxy(startX * (squareSize + 1) + 1, row + 1);
+        gotoxy(startX * (squareSize) + 1, row+1);
         
         // Print the first and last rows with '*' characters
-        if (row == startY || row == startY+(squareSize - 1)) {
+        if (row == startY* (squareSize + 1) || row == startY*(squareSize + 1)+(squareSize - 1)) {
             for (int col = 0; col < squareSize; ++col) {
                 cout << "*";
             }
@@ -21,7 +21,7 @@ void printSquare(int startX,int startY, char character, int color) {
             // Print the middle rows with '*' at the edges and spaces inside
             cout << "*";
             for (int col = startX+1; col < startX+(squareSize - 1); ++col) {
-                if(row == startY+(squareSize/2) && col == startX+(squareSize/2)){
+                if(row == startY*(squareSize + 1)+(squareSize/2) && col == startX+(squareSize/2)){
                     cout << character;
                 } else {
                     cout << " ";
@@ -33,18 +33,13 @@ void printSquare(int startX,int startY, char character, int color) {
 }
 
 void printRow() {
-    // printSquare(0,0,'A');
-    // printSquare(1,0,'A');
-    // printSquare(2,0,'A');
-    // printSquare(3,0,'A');
-    // printSquare(4,0,'A');
-
-    printSquare(0,0,'A',RED);
-    printSquare(0,5,'A',GREEN);
-    printSquare(2,10,'A',YELLOW);
-    printSquare(3,15,'A',WHITE);
-    printSquare(4,20,'A',RED);
-    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printSquare(i,j,(65+i+j),i);
+        }   
+    }
 }
 
 int main() {
