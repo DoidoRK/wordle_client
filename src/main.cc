709141a89cc7d2edf0bf98d3e2e5f3554750d3ef
João Carlos <doidobr46@gmail.com>
2023-10-22 15:16:29 -0300

    user GUI working as expected

--- src/main.cc
+++ src/main.cc
@@ -23,7 +23,6 @@ pthread_t user_input_thread, timer_thread, gui_thread;
 // Mutex for accessing shared data
 pthread_mutex_t user_attempt_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t user_input_detected = PTHREAD_COND_INITIALIZER;
 
 using namespace std;
 
@@ -32,10 +31,10 @@ string user_input_string;
 int current_row = 0;
 int current_col = 0;
 
-void showAttemptMessage(string message){
+void showAttemptMessage(char message[]){
     pthread_mutex_lock(&print_mutex);
     mvprintw(7, 0, "%s", message);
-    // mvprintw(7, 0, "Pressione enter para enviar a palavra");
+    refresh();
     pthread_mutex_unlock(&print_mutex);
 }
 
@@ -43,12 +42,7 @@ void clearAttemptMessage(){
     pthread_mutex_lock(&print_mutex);
     move(7, 0);
     clrtoeol();
-    pthread_mutex_unlock(&print_mutex);
-}
-
-void printUserInput(int curent_row, int current_col, char character){
-    pthread_mutex_lock(&print_mutex);
-    mvprintw(current_row, current_col, "%c", character);
+    refresh();
     pthread_mutex_unlock(&print_mutex);
 }
 
@@ -58,24 +52,22 @@ void * userInputThread(void * args) {
     const int num_cols = WORD_SIZE;
     while (!quit_program) {
         int ch = getch();
-        pthread_cond_signal(&user_input_detected);
+        pthread_mutex_lock(&user_attempt_mutex);
         if (ch == 27) {  // ESC
             quit_program = true;
         } else if (ch == 10 && current_col == num_cols) {  // Enter key
-            pthread_mutex_lock(&user_attempt_mutex);
             sendAttemptToServer(&current_row, &current_col , &player, &current_attempt,user_input_string, attempts);
-            pthread_mutex_unlock(&user_attempt_mutex);
             clearAttemptMessage();
         } else if (ch == KEY_BACKSPACE) {  // Backspace key
             if (!user_input_string.empty() || current_col > 0) {
                 user_input_string.pop_back();
+                attempts[current_attempt].word[current_col-1] = '-';
                 --current_col;
-                printUserInput(current_row, current_col, '-');
                 clearAttemptMessage();
             }
         } else if(isalpha(ch) && current_col < num_cols) {
             user_input_string.push_back(ch);
-            printUserInput(current_row, current_col, ch);
+            attempts[current_attempt].word[current_col] = ch;
             current_col++;
             clearAttemptMessage();
         }
@@ -85,7 +77,7 @@ void * userInputThread(void * args) {
         if (current_row == num_rows){
             quit_program = true;
         }
-        refresh();
+        pthread_mutex_unlock(&user_attempt_mutex);
     }
     return NULL;
 }
@@ -97,12 +89,12 @@ void * displayTimer(void *args) {
     {
         pthread_mutex_lock(&print_mutex);
         mvprintw(0, 15, "Timer: %02d", (190 - (seconds % 60)));
+        refresh();
         pthread_mutex_unlock(&print_mutex);
         // If timer == 0
         //Sends new word message for the server.
         //Cleans up user attempts.
         //Tells the user that the time is up in bottom message.
-        refresh();
         sleep(1);
         seconds++;
     }
@@ -111,59 +103,62 @@ void * displayTimer(void *args) {
 
 void * displayGUIThread(void *args){
     int attempts_printed = 0;
-    initializeAttempts(attempts,WORD_SIZE);
+    int word_size = WORD_SIZE;
+    initializeAttempts(attempts, word_size);
     while (!quit_program) {
         pthread_mutex_lock(&print_mutex);
-        pthread_cond_wait(&user_input_detected, &print_mutex);
-        printTries(attempts,WORD_SIZE);
-        pthread_mutex_unlock(&print_mutex);
+        printTries(attempts, word_size);
+        refresh();
         attempts_printed++;
+        pthread_mutex_unlock(&print_mutex);
     }
     return NULL;
 }
 
 int main() {
-    loginPlayer(&player);   // Logs player in
-    // initscr();              // Initialize ncurses
-    // start_color();          // Habilita cores no terminal
+    loginPlayer(&player);   // Logs player.
+    cout << "Player logado!";
+    sleep(1);
+    initscr();              // Initialize ncurses
+    start_color();          // Habilita cores no terminal
 
-    // cbreak();               // Line buffering disabled
-    // noecho();               // Don't echo user input
-    // keypad(stdscr, TRUE);   // Enable special keys
-    // curs_set(0);            // Hide cursors
+    cbreak();               // Line buffering disabled
+    noecho();               // Don't echo user input
+    keypad(stdscr, TRUE);   // Enable special keys
+    curs_set(0);            // Hide cursors
 
-    // if (pthread_create(&user_input_thread, NULL, userInputThread, NULL) != 0) {
-    //     endwin();
-    //     fprintf(stderr, "Failed to create the input thread\n");
-    //     return 1;
-    // }
+    if (pthread_create(&user_input_thread, NULL, userInputThread, NULL) != 0) {
+        endwin();
+        fprintf(stderr, "Failed to create the input thread\n");
+        return 1;
+    }
 
-    // if (pthread_create(&gui_thread, NULL, displayGUIThread, NULL) != 0) {
-    //     endwin();
-    //     fprintf(stderr, "Failed to create the GUI thread\n");
-    //     return 1;
-    // }
+    if (pthread_create(&gui_thread, NULL, displayGUIThread, NULL) != 0) {
+        endwin();
+        fprintf(stderr, "Failed to create the GUI thread\n");
+        return 1;
+    }
 
-    // if (pthread_create(&timer_thread, NULL, displayTimer, NULL) != 0) {
-    //     endwin();
-    //     fprintf(stderr, "Failed to create the timer thread\n");
-    //     return 1;
-    // }
+    if (pthread_create(&timer_thread, NULL, displayTimer, NULL) != 0) {
+        endwin();
+        fprintf(stderr, "Failed to create the timer thread\n");
+        return 1;
+    }
 
-    // if (pthread_join(user_input_thread, NULL) != 0) {
-    //     fprintf(stderr, "Failed to join the input thread\n");
-    //     return 1;
-    // }
+    if (pthread_join(user_input_thread, NULL) != 0) {
+        fprintf(stderr, "Failed to join the input thread\n");
+        return 1;
+    }
 
-    // if (pthread_join(gui_thread, NULL) != 0) {
-    //     fprintf(stderr, "Failed to join the GUI thread\n");
-    //     return 1;
-    // }
+    if (pthread_join(gui_thread, NULL) != 0) {
+        fprintf(stderr, "Failed to join the GUI thread\n");
+        return 1;
+    }
 
-    // if (pthread_join(timer_thread, NULL) != 0) {
-    //     fprintf(stderr, "Failed to join the timer thread\n");
-    //     return 1;
-    // }
+    if (pthread_join(timer_thread, NULL) != 0) {
+        fprintf(stderr, "Failed to join the timer thread\n");
+        return 1;
+    }
 
     endwin();  // Terminate ncurses
     return 0;
